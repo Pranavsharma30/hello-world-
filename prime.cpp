8103316eeb429044c23226ae1a11a395dee1567f
Pranavsharma30 <138870344+Pranavsharma30@users.noreply.github.com>
2023-08-03 10:55:50 +0530

    Add files via upload

---
+++ prime.cpp
@@ -0,0 +1,21 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool Prime(int n)
+{
+	if (n <= 1)
+		return false;
+	for (int i = 2; i <= n / 2; i++)
+		if (n % i == 0)
+			return false;
+	return true;
+}
+int main()
+{
+    int x;
+    cout<<"enter the no"<<endl;
+    cin>>x;
+	Prime(x) ? cout <<endl<< x << " is a prime no \n" : cout << x << " is not a prime no\n";
+	return 0;
+}
